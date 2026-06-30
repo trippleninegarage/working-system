cec5cf77edc8712a1825ee84e9f68bfcaae1c00a
trippleninegarage <trippleninegarage@gmail.com>
2026-07-01 02:02:33 +0700

    Update login.js

--- login.js
+++ login.js
@@ -11,7 +11,7 @@ async function handleLogin() {
     const data = await response.json();
 
     if (data.success) {
-        localStorage.setItem('userName', user); // บันทึกชื่อ
+        localStorage.setItem('userName', 'ชื่อเล่นของคุณ'); // เช่น 'ต้น'
         localStorage.setItem('userRole', data.role); // บันทึกตำแหน่ง
         window.location.href = 'home.html';
     } else {
